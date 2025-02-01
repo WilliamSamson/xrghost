cc89dc57b63420aae5beab54027024a4aed6d030
William samson <99078154+WilliamSamson@users.noreply.github.com>
2025-02-01 17:01:58 +0100

    deploy ready
    made first deploy

--- lib/Interface/Home/home.dart
+++ lib/Interface/Home/home.dart
@@ -5,33 +5,37 @@ import 'package:xrghost/Interface/Home/web_footer.dart';
 import 'package:xrghost/core/imports.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'file_design.dart';
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final isMobile = screenWidth < 600;
+    final isTablet = screenWidth >= 600 && screenWidth < 1200;
+    final isPc = screenWidth >= 1400;
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 24),
+        padding: EdgeInsets.symmetric(
+          horizontal: isMobile ? 16 : 32,
+          vertical: isMobile ? 12 : 24,
+        ),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-
-            // Left Navigation Rail
-            _buildNavigationRail(context),
+            // Left Navigation Rail (Hidden on Mobile)
+            if (!isMobile) _buildNavigationRail(context),
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    _buildTop(context),
-                    _buildHeaderSection(context),
+                    _buildTop(context, isMobile, isTablet),
+                    _buildHeaderSection(context, isMobile, isTablet),
                     const SizedBox(height: 40),
-                    // Team Section
-                    _buildTeamSection(context),
+                    _buildTeamSection(context, isMobile, isTablet),
                     WebsiteFooter(),
                   ],
                 ),
@@ -43,74 +47,168 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _buildTop(BuildContext context) {
-    return Row(
+  Widget _buildTop(BuildContext context, bool isMobile, bool isTablet) {
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        ProfileCard(),
-        Padding(
-          padding: const EdgeInsets.only(left: 20, right: 10, top: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Portfolio',
-                style: GoogleFonts.ubuntu(
-                  fontSize: 80,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 10),
-              SizedBox(
-                width: 350, // Ensures the width is constrained
-                height: 300, // Adjust based on the required height
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: StaggeredGrid.count(
-                    crossAxisCount: 3,
-                    mainAxisSpacing: 8,
-                    crossAxisSpacing: 8,
-                    children: const [
-                      StaggeredGridTile.fit(
-                        crossAxisCellCount: 2,
-                        child: _StatCards(
-                          number: '20+',
-                          label: 'Projects',
-                          color: Colors.blue,
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // ProfileCard with scaling for mobile
+            isMobile
+                ? Expanded(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Transform.scale(scale: 1, child: ProfileCard()),
+                    ),
+                  )
+                : isTablet
+                    ? Transform.scale(
+                        scale: isTablet ? 1.0 : 1.05, child: ProfileCard())
+                    : Expanded(
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 20, top: 20,right: 20),
+                            child: Transform.scale(
+                                scale: 1.2, child: ProfileCard()),
+                          ),
                         ),
                       ),
-                      StaggeredGridTile.fit(
-                        crossAxisCellCount: 1,
-                        child: _StatCards(
-                          number: '10+',
-                          label: 'Clients',
-                          color: Colors.green,
+            if (!isMobile) // Only show the Portfolio section on non-mobile screens
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 10, top: 10),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Portfolio',
+                        style: GoogleFonts.ubuntu(
+                          fontSize: isTablet ? 60 : 80,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
                         ),
                       ),
-                      StaggeredGridTile.fit(
-                        crossAxisCellCount: 2,
-                        child: _StatCards(
-                          number: '3',
-                          label: 'Hackathons',
-                          color: Colors.orange,
-                        ),
-                      ),
-                      StaggeredGridTile.fit(
-                        crossAxisCellCount: 1,
-                        child: _StatCards(
-                          number: '3',
-                          label: 'Collabs',
-                          color: Colors.purple,
+                      const SizedBox(height: 10),
+                      SizedBox(
+                        width: isTablet
+                            ? MediaQuery.of(context).size.width * 0.4
+                            : MediaQuery.of(context).size.width * 0.6,
+                        height: isTablet ? 350 : 400,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: StaggeredGrid.count(
+                            crossAxisCount: isTablet ? 2 : 3,
+                            mainAxisSpacing: 8,
+                            crossAxisSpacing: 8,
+                            children: const [
+                              StaggeredGridTile.fit(
+                                crossAxisCellCount: 2,
+                                child: _StatCards(
+                                  number: '20+',
+                                  label: 'Projects',
+                                  color: Colors.blue,
+                                ),
+                              ),
+                              StaggeredGridTile.fit(
+                                crossAxisCellCount: 1,
+                                child: _StatCards(
+                                  number: '10+',
+                                  label: 'Clients',
+                                  color: Colors.green,
+                                ),
+                              ),
+                              StaggeredGridTile.fit(
+                                crossAxisCellCount: 2,
+                                child: _StatCards(
+                                  number: '3',
+                                  label: 'Hackathons',
+                                  color: Colors.orange,
+                                ),
+                              ),
+                              StaggeredGridTile.fit(
+                                crossAxisCellCount: 1,
+                                child: _StatCards(
+                                  number: '3',
+                                  label: 'Collabs',
+                                  color: Colors.purple,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ],
                   ),
                 ),
               ),
-            ],
-          ),
+          ],
         ),
+        if (isMobile) // Show Portfolio section below ProfileCard on mobile
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Portfolio',
+                  style: GoogleFonts.ubuntu(
+                    fontSize: 60,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: double.infinity,
+                  height: 450,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: StaggeredGrid.count(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 8,
+                      crossAxisSpacing: 8,
+                      children: const [
+                        StaggeredGridTile.fit(
+                          crossAxisCellCount: 2,
+                          child: _StatCards(
+                            number: '20+',
+                            label: 'Projects',
+                            color: Colors.blue,
+                          ),
+                        ),
+                        StaggeredGridTile.fit(
+                          crossAxisCellCount: 1,
+                          child: _StatCards(
+                            number: '10+',
+                            label: 'Clients',
+                            color: Colors.green,
+                          ),
+                        ),
+                        StaggeredGridTile.fit(
+                          crossAxisCellCount: 2,
+                          child: _StatCards(
+                            number: '3',
+                            label: 'Hackathons',
+                            color: Colors.orange,
+                          ),
+                        ),
+                        StaggeredGridTile.fit(
+                          crossAxisCellCount: 1,
+                          child: _StatCards(
+                            number: '3',
+                            label: 'Collabs',
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
       ],
     );
   }
@@ -120,30 +218,35 @@ class HomePage extends StatelessWidget {
       padding: const EdgeInsets.only(right: 40),
       child: RotatedBox(
         quarterTurns: -1,
-        child:  Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 15),
-    child:Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            _NavItem(title: 'Channel', onTap: () {}),
-            _NavItem(title: 'Portfolio', onTap: () {}),
-            _NavItem(title: 'Research', onTap: () {}),
-            _NavItem(title: 'Clients', onTap: () {}),
-          ],
-        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _NavItem(title: 'Channel', onTap: () {}),
+              _NavItem(title: 'Portfolio', onTap: () {}),
+              _NavItem(title: 'Research', onTap: () {}),
+              _NavItem(title: 'Clients', onTap: () {}),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildHeaderSection(BuildContext context) {
+  Widget _buildHeaderSection(
+      BuildContext context, bool isMobile, bool isTablet) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           'Building digital products,\nbrands, and experience.',
           style: GoogleFonts.ubuntu(
-            fontSize: 42,
+            fontSize: isMobile
+                ? 28
+                : isTablet
+                    ? 36
+                    : 42,
             fontWeight: FontWeight.bold,
             height: 1.2,
           ),
@@ -152,7 +255,7 @@ class HomePage extends StatelessWidget {
         Text(
           'Collaborate with brands and agencies\nto create impactful results.',
           style: GoogleFonts.ubuntu(
-            fontSize: 18,
+            fontSize: isMobile ? 14 : 18,
             color: Colors.grey[600],
           ),
         ),
@@ -160,14 +263,14 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _buildTeamSection(BuildContext context) {
+  Widget _buildTeamSection(BuildContext context, bool isMobile, bool isTablet) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           'Core Team',
           style: GoogleFonts.ubuntu(
-            fontSize: 18,
+            fontSize: isMobile ? 16 : 18,
             fontWeight: FontWeight.w500,
           ),
         ),
